29c571f84dd7fe59d78c38c9fc8b8b7c5b564fce
Pavishy <78425614+Pavishy@users.noreply.github.com>
2021-10-28 14:26:40 -0500

    Initial commit

---
+++ CadenasMarkov.py
@@ -0,0 +1,30 @@
+import numpy as n
+
+T = [0.6,0.2,0.2],[0.3,0.4,0.3],[0.1,0.4,0.5]
+P_inicial = [0.7,0.2,0.1]
+
+matrizT = n.array(T)
+matriz_P0 = n.array(P_inicial)
+
+print(matrizT)
+print()
+print(matriz_P0)
+
+print("P1: ")
+P1 = matriz_P0.dot(matrizT)
+print(P1)
+
+print("P2: ")
+P2 = P1.dot(matrizT)
+print(P2)
+
+estadoDeseado = int(input("Ingresa el estado que se desea: "))
+print("Estado buscado: " + str(estadoDeseado))
+
+estadoActual = matriz_P0
+for i in range(estadoDeseado):
+    estadoActual = estadoActual.dot(matrizT)
+    print ("P" +str(i+1)+": "+ str(estadoActual))
+
+print("Resultado: (P"+str(estadoDeseado)+"): ")
+print(estadoActual)
---
+++ Inv_Operaciones_con_string.py
@@ -0,0 +1,21 @@
+saludo = "Buenos dias"
+saludo+= ", "
+saludo +="Andres"
+saludo +="."
+
+print(saludo)
+
+texto = "Hola. "
+
+print(texto*5)
+
+cadena = "Hola desde \"Python\""
+
+print(cadena[0:4])
+print(cadena[0:10])
+
+buscar = "Hola"
+texto = "Hola desde Python"
+print(buscar in texto) #True si se encuentra en el texto
+
+print(buscar not in texto) #False no se encuentra en el texto
